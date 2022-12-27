a984d451930bd21149ce195e7e31edcb0f8baf9a
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2022-12-27 00:51:29 -0300

    feat: tag music after download

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,24 @@
-import YoutubeMp3Downloader from "youtube-mp3-downloader";
+import fs from "fs";
+import nodeID3 from "node-id3";
 import prompt from "prompt-sync";
+import YoutubeMp3Downloader from "youtube-mp3-downloader";
+import { music } from "./types/music";
 
-const input = prompt();
-const inputVideosIds = input("Insert the videos IDs separated by comma:");
-const inputTargetPath = input(
-  "Insert the folder absolute path to save MP3 files:"
-);
+async function main() {
+  const input = prompt();
+  const inputMusicListFilePath = input("Insert the music list file path:");
+  const inputTargetPath = input(
+    "Insert the folder absolute path to save MP3 files:"
+  );
+  const targetPath = inputTargetPath || __dirname.replace("/src", "");
+  const rawMusicList = fs.readFileSync(inputMusicListFilePath);
+  const musicList: music[] = JSON.parse(rawMusicList.toString());
 
-if (inputVideosIds) {
-  const videosIds = inputVideosIds.split(",");
   const youtubeMp3Downloader = new YoutubeMp3Downloader({
-    outputPath: inputTargetPath || __dirname.replace("/src", ""),
+    outputPath: targetPath,
     progressTimeout: 2000,
-    queueParallelism: 1,
+    queueParallelism: 10,
     youtubeVideoQuality: "highestaudio",
-  });
-  youtubeMp3Downloader.on("finished", function (err, data) {
-    console.log(`Finished downloading \"${data.videoTitle}\"`);
   });
   youtubeMp3Downloader.on("error", function (error) {
     console.log(error);
@@ -26,10 +28,32 @@ if (inputVideosIds) {
       `${progress.videoId} - ${progress.progress.percentage.toFixed(2)}%`
     );
   });
+  youtubeMp3Downloader.on("finished", function (err, data) {
+    console.log(`Finished downloading \"${data.videoTitle}\"`);
+  });
+  youtubeMp3Downloader.on("queueSize", function (size) {
+    if (size === 0) {
+      for (const music of musicList) {
+        console.log(`Tagging ${music.path}`);
+        nodeID3.write(
+          { title: music.title, artist: music.artist },
+          music.path || ""
+        );
+      }
+    }
+  });
 
-  for (const videoId of videosIds) {
-    youtubeMp3Downloader.download(videoId);
+  for (const music of musicList) {
+    const regexVideoId = /(?<=watch\?v=).*?(?=&|$)/g;
+    const musicUrlVideoIdMatch = music.url.match(regexVideoId);
+    const videoId = musicUrlVideoIdMatch ? musicUrlVideoIdMatch[0] : "";
+    const fileName = `${music.title} - ${music.artist}.mp3`;
+    const filePath = `${targetPath}/${fileName}`;
+
+    music.path = filePath;
+
+    youtubeMp3Downloader.download(videoId, fileName);
   }
-} else {
-  throw Error("No input video was given");
 }
+
+main();
---
+++ src/types/music.ts
@@ -0,0 +1,7 @@
+export type music = {
+  url: string;
+  title: string;
+  artist: string;
+  album?: string;
+  path?: string;
+};
